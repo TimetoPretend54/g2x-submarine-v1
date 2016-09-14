4df29a8cd0d4a1660c49360026da3363be03a115
Kevin Lindsey <kevin@kevlindev.com>
2016-09-13 17:54:14 -0700

    Simple display of an object’s properties
    This includes a conditional import so we can run on raspbery pi and on mac os

--- scratchpad/ncurses.py
+++ scratchpad/ncurses.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
 from curses import wrapper
+import platform
 
+if platform.system() == "Darwin":
+	# create mock class for Pi Camera
+	class Camera:
+		def __init__(self):
+			self.brightness = 10
+			self.contrast = 24
+else:
+	import picamera
+
+properties = [
+	"brightness",
+	"contrast"
+]
+
+camera = Camera()
 
 def main(stdscr):
 	# clear screen
 	stdscr.clear()
+
+	row = 0
+	for key in properties:
+		value = getattr(camera, key)
+		string = key + ": " + str(value)
+		stdscr.addstr(row, 0, string)
+		row = row + 1
 
 	stdscr.refresh()
 	stdscr.getkey()
