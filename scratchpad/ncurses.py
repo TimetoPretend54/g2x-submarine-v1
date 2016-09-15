3ecab77c2fc65871fd879cf9d1e5806c45102af4
Kevin Lindsey <kevin@kevlindev.com>
2016-09-15 14:37:24 -0700

    A little bit of code cleanup

--- scratchpad/ncurses.py
+++ scratchpad/ncurses.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
 import platform
 import signal
 import shutil
 import time
 import curses
 # from PIL import Image
+import logging
 
 if platform.system() == "Darwin":
     # use mock classes
@@ -15,27 +15,39 @@ else:
     # assume we're on a raspberry pi
     from picamera import PiCamera
 
+# setup logging
+# timestamp = int(time.time())
+# filename = "g2x-{}.log".format(timestamp)
+logging.basicConfig(filename="g2x.log", level=logging.DEBUG)
+
+# config
+use_overlay = False
+preview_window = (0, 0, 640, 480)
+camera_resolution = (1296, 972)  # "1080p"
+camera_framerate = 24
+
 # create access to camera
-camera = PiCamera()
+camera = PiCamera(resolution=camera_resolution, framerate=camera_framerate)
 
 # create overlays
-# img0 = Image.open('recording.png')
-# pad0 = Image.new('RGB', (
-#     ((img0.size[0] + 31) // 32) * 32,
-#     ((img0.size[1] + 15) // 16) * 16))
-# pad0.paste(img0, (0, 0), img0)
-# o0 = camera.add_overlay(pad0.tostring(), size=img0.size)
-# o0.layer = 3
-# o0.alpha = 0
+if use_overlay:
+    img0 = Image.open('recording.png')
+    pad0 = Image.new('RGB', (
+        ((img0.size[0] + 31) // 32) * 32,
+        ((img0.size[1] + 15) // 16) * 16))
+    pad0.paste(img0, (0, 0), img0)
+    o0 = camera.add_overlay(pad0.tostring(), size=img0.size)
+    o0.layer = 3
+    o0.alpha = 0
 
-# img1 = Image.open('not-recording.png')
-# pad1 = Image.new('RGB', (
-#     ((img1.size[0] + 31) // 32) * 32,
-#     ((img1.size[1] + 15) // 16) * 16))
-# pad1.paste(img1, (0, 0), img1)
-# o1 = camera.add_overlay(pad1.tostring(), size=img1.size)
-# o1.layer = 4
-# o1.alpha = 0
+    img1 = Image.open('not-recording.png')
+    pad1 = Image.new('RGB', (
+        ((img1.size[0] + 31) // 32) * 32,
+        ((img1.size[1] + 15) // 16) * 16))
+    pad1.paste(img1, (0, 0), img1)
+    o1 = camera.add_overlay(pad1.tostring(), size=img1.size)
+    o1.layer = 4
+    o1.alpha = 0
 
 # create list of properties to display
 properties = [
@@ -99,18 +111,18 @@ def update_screen(stdscr):
         # just in case we have an invalid property, wrap this iteration in
         # a try/except statement
         try:
+            # NOTE: we may need to handle the case where the current string
+            # cannot fit in the current row
+
             # display key/value on screen
-            # NOTE: we probably need to handle the case where the current
-            # string cannot fit in the current row
             value = getattr(camera, key)
             keystring = key.rjust(max_len) + ": "
             stdscr.addstr(row, col, keystring)
             valuestring = str(value)
-            string = keystring + valuestring
             stdscr.addstr(row, col + len(keystring), valuestring, curses.A_BOLD)
 
             # update max_col
-            current_end_col = col + len(string)
+            current_end_col = col + len(keystring) + len(valuestring)
 
             if current_end_col > max_col:
                 max_col = current_end_col
@@ -126,9 +138,11 @@ def update_screen(stdscr):
                 if col >= curses.COLS:
                     break
         except:
-            print("An exception occurred", file=sys.stderr)
+            # TODO: figure out how to include the exception in the output
+            logging.error("An exception occurred")
 
     # move the cursor to a sensible location and update the screen
+    stdscr.addstr(curses.LINES - 1, 0, "[p]review [q]uit [r]ecord [-/=]brightness [_/+]contrast", curses.A_BOLD)
     stdscr.move(curses.LINES - 1, 0)
     stdscr.refresh()
 
@@ -169,7 +183,7 @@ def main(stdscr):
         elif ch == ord("p"):
             preview = not preview
             if preview:
-                camera.start_preview()
+                camera.start_preview(fullscreen=False, window=preview_window)
             else:
                 camera.stop_preview()
         elif ch == ord("r"):
@@ -178,17 +192,23 @@ def main(stdscr):
                 timestamp = int(time.time())
                 filename = "g2x-{}.h264".format(timestamp)
                 camera.start_recording(filename)
-                # o0.alpha = 255
-                # o1.alpha = 0
-                camera.annotate_text = "Recording"
-                # camera.annotate_text = "1234567890" * 4
+
+                if use_overlay:
+                    o0.alpha = 255
+                    o1.alpha = 0
+                else:
+                    camera.annotate_text = "Recording"
+                    # camera.annotate_text = "1234567890" * 4
             else:
                 camera.stop_recording()
-                # o0.alpha = 0
-                # o1.alpha = 255
-                camera.annotate_text = ""
+
+                if use_overlay:
+                    o0.alpha = 0
+                    o1.alpha = 255
+                else:
+                    camera.annotate_text = ""
         else:
-            print("Unhandled key: " + str(ch), file=sys.stderr)
+            logging.debug("Unhandled key: " + str(ch))
 
 
 try:
