c9ad7aa6c40eb5fec042dc540a27c822cbc689cf
Kevin Lindsey <kevin@kevlindev.com>
2016-09-26 11:46:30 -0700

    Clamp frequency, channel, on, and off input values

--- app/PWM/pwm_controller.py
+++ app/PWM/pwm_controller.py
@@ -6,7 +6,18 @@ class PWMController:
         self.pwm = Adafruit_PCA9685.PCA9685()
 
     def set_frequency(self, freq):
+        # [40,1000] comes from Adafruit docs
+        freq = max(40, min(freq, 1000))
+
         self.pwm.set_pwm_freq(freq)
 
+    # on/off are in ticks (based on freq)
     def set_pwm(self, channel, on, off):
+        # [0,15] comes from Adafruit docs
+        channel = max(0, min(channel, 15))
+
+        # [0,4095] comes from Adafruit docs
+        on = max(0, min(on, 4095))
+        off = max(0, min(off, 4095))
+
         self.pwm.set_pwm(channel, on, off)
