0de0818e5a0c52dde8c841d8e8254e2f4a3f9633
Kevin Lindsey <kevin@kevlindev.com>
2016-09-26 10:23:00 -0700

    Allow PiSense readings to be toggled on/off

--- app/sense.py
+++ app/sense.py
@@ -8,7 +8,7 @@ import time
 
 
 DEVICE = "PiSense"
-DELAY = 0.0
+DELAY = 0.25
 
 
 class Handler:
@@ -16,30 +16,47 @@ class Handler:
         self.display = display
         self.logger = logger
         self.sensor = sensor
+        self.recording = False
         self.logger.log(DEVICE, "running", 1)
 
     def read(self):
         values = {}
 
-        for reading in self.sensor.get_data():
-            values[reading[1]] = reading[2]
-            self.logger.log(DEVICE, reading[1], reading[2], reading[0])
+        if self.recording:
+            for reading in self.sensor.get_data():
+                values[reading[1]] = reading[2]
+                self.logger.log(DEVICE, reading[1], reading[2], reading[0])
+            display.show_properties(values, self.sensor.get_properties())
+        else:
+            values["recording"] = False
+            display.show_properties(values)
 
-        display.show_properties(values, self.sensor.get_properties())
+        return True
+
+    def record(self):
+        self.recording = not self.recording
+
+        if self.recording:
+            self.logger.log(DEVICE, "recording", 1)
+        else:
+            self.logger.log(DEVICE, "recording", 0)
+
         return True
 
     def quit(self):
         self.logger.log(DEVICE, "running", 0)
+
         return False
 
 
 with SenseController() as sensor, KeyDispatcher() as dispatcher, SQLiteLogger() as logger:
     # setup display
-    display = Display("PiSense")
+    display = Display("PiSense", "[r]ecord [q]uit")
 
     # setup key handlers
     handler = Handler(display, logger, sensor)
 
+    dispatcher.add("r", handler, "record")
     dispatcher.add("q", handler, "quit")
 
     # start processing key presses
