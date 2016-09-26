0667072d7b34f8fa6e20d7fd08e446641c82b03f
Kevin Lindsey <kevin@kevlindev.com>
2016-09-25 17:08:39 -0700

    Allow property order to be defined when displaying property list

--- app/Display/display.py
+++ app/Display/display.py
@@ -16,13 +16,17 @@ class Display:
     def clear(self):
         sys.stdout.write(ERASE)
 
-    def show_properties(self, properties):
+    def show_properties(self, properties, names=None):
         self.clear()
         self.print(self.title)
         print()
 
-        for k, v in properties.items():
-            self.print("{0}: {1}".format(k, v))
+        if names is None:
+            for k, v in properties.items():
+                self.print("{0}: {1}".format(k, v))
+        else:
+            for k in names:
+                self.print("{0}: {1}".format(k, properties[k]))
 
     def print(self, message):
         print(message, end="\x0a\x0d")
