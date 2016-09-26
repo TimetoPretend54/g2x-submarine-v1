4de56b81cbfebabe8deca3c24deb48c69ff92f78
Kevin Lindsey <kevin@kevlindev.com>
2016-09-25 17:58:36 -0700

    Right justify property names

--- app/Display/display.py
+++ app/Display/display.py
@@ -17,16 +17,17 @@ class Display:
         sys.stdout.write(ERASE)
 
     def show_properties(self, properties, names=None):
+        if names is None:
+            names = properties.keys()
+
+        max_len = max(map(len, names))
+
         self.clear()
         self.print(self.title)
         print()
 
-        if names is None:
-            for k, v in properties.items():
-                self.print("{0}: {1}".format(k, v))
-        else:
-            for k in names:
-                self.print("{0}: {1}".format(k, properties[k]))
+        for k in names:
+            self.print("{0}: {1}".format(k.rjust(max_len), properties[k]))
 
     def print(self, message):
         print(message, end="\x0a\x0d")
