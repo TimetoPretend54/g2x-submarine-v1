94b468fa101a9ea1eab70e35faed24359d0b8e3a
Kevin Lindsey <kevin@kevlindev.com>
2016-10-21 13:46:38 -0700

    Set initial value for thruster

--- app/pwm.py
+++ app/pwm.py
@@ -120,7 +120,7 @@ with KeyDispatcher() as dispatcher, SQLiteLogger() as logger:
 
     # [1100µs,1900µs] = [271,467] @ 60Hz
     handler.add_device("PWM Light", 2, 0, 320)
-    handler.add_device("PWM Thruster", 1, 0, 271)
+    handler.add_device("PWM Thruster", 1, 0, 400)
 
     dispatcher.add("p", handler, "previous_device")
     dispatcher.add("n", handler, "next_device")
