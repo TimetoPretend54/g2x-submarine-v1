bddfaae796ed1672771259b88dc45b1257f721c4
Kevin Lindsey <kevin@Ronans-MacBook-Air.local>
2016-11-03 13:54:23 -0700

    Display voltage reading in volts

--- app/Voltage/voltage.py
+++ app/Voltage/voltage.py
@@ -3,13 +3,25 @@ import time
 
 ADDRESS = 4
 CMD_READ_ANALOG = 1
+VOLT12 = 650
+VOLT18 = 978
 
+def map_range(x, in_min, in_max, out_min, out_max):
+    out_delta = out_max - out_min
+    in_delta = in_max - in_min
+
+    return (x - in_min) * out_delta / in_delta + out_min
 
 class Voltage:
     def __init__(self):
         self.bus = smbus.SMBus(1)
 
     def get_data(self):
+        voltage_time = time.time();
+        voltage_raw = self.bus.read_word_data(ADDRESS, CMD_READ_ANALOG)
+        voltage = map_range(voltage_raw, VOLT12, VOLT18, 12, 18)
+
         return [
-            (time.time(), "voltage", self.bus.read_word_data(ADDRESS, CMD_READ_ANALOG))
+            (voltage_time, "voltage_raw", voltage_raw),
+            (voltage_time, "voltage", voltage)
         ]
