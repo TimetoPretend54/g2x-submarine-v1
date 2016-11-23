3525402fb7da2231ab880a27b3fe90d99878bdfb
Kevin Lindsey <kevin@kevlindev.com>
2016-11-23 13:53:46 -0800

    First pass at rendering overlays on all frames
    For now, the directory containing the frames is hard-coded

--- overlay/composite-frame.py
+++ overlay/composite-frame.py
@@ -1,25 +1,76 @@
 #!/usr/bin/env python3
 
+import os
+import time
+import io
+import re
+
+import pystache
 from PIL import Image
 import cairosvg
-import io
 
 
-# load frame
-frame = Image.open('./test-frames/g2x-1479068752-0000.tif', 'r')
+class Data:
+    def __init__(self, secs_since_epoch):
+        self.time = time.localtime(secs_since_epoch)
 
-# load overlay
-overlay_bytes = cairosvg.svg2png(
-    url='./test-overlays/g2x-1479068752-0000.svg'
-)
-overlay = Image.open(io.BytesIO(overlay_bytes))
+    def frame_date(self):
+        return time.strftime("%B %d, %Y", self.time)
 
-# create composite image holde
-composite = Image.new('RGB', frame.size, (255, 255, 255, 255))
+    def frame_time(self):
+        return time.strftime("%I:%M:%S %p", self.time)
 
-# composite images
-composite.paste(frame)
-composite.paste(overlay, (0, 0), overlay)
 
-# output result
-composite.save('./test-composites/g2x-1479068752-0000.png')
+# create renderer
+renderer = pystache.Renderer()
+
+# load template
+template_file = open('./overlay.svg.mustache')
+template = template_file.read()
+template_file.close()
+
+# process all files
+frame_dir = '/Users/kevin/Desktop/Second Dive/g2x-1479068752/'
+composite_dir = '/Users/kevin/Desktop/Second Dive/g2x-1479068752-composites/'
+
+# create regex for extracting time data from file names
+time_and_frame_pattern = re.compile(r"(\d+)-(\d+)$")
+
+# process all frames in the frame directory
+for frame_file in os.listdir(frame_dir):
+    if frame_file == ".DS_Store":
+        continue
+
+    # extract frame's time from file name
+    frame_no_ext = os.path.splitext(frame_file)[0]
+    file_match = time_and_frame_pattern.search(frame_no_ext)
+    base_time = float(file_match.group(1))
+    frame_number = float(file_match.group(2))
+    frame_time = base_time + frame_number / 24
+
+    print("processing frame {0}...".format(str(int(frame_number))))
+
+    # load frame
+    frame_full_path = frame_dir + frame_file
+    frame = Image.open(frame_full_path, 'r')
+
+    # load data
+    frame_data = Data(frame_time)
+
+    # render SVG text
+    svg = renderer.render(template, frame_data)
+
+    # create overlay image from SVG
+    overlay_bytes = cairosvg.svg2png(bytestring=svg)
+    overlay = Image.open(io.BytesIO(overlay_bytes))
+
+    # create composite image holder
+    composite = Image.new('RGB', frame.size, (255, 255, 255))
+
+    # composite images
+    composite.paste(frame)
+    composite.paste(overlay, (0, 0), overlay)
+
+    # output result
+    composite_full_path = composite_dir + frame_no_ext + ".png"
+    composite.save(composite_full_path, optimize=False)
