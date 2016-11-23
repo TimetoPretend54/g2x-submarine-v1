83547f4fd39ce30d9a8836529a95c37b522c3431
Kevin Lindsey <kevin@kevlindev.com>
2016-11-23 14:20:48 -0800

    Add argument parsing
    This allows us to indicate where the source frame images reside. We also have to specify the output directory. Optionally starting and ending frame numbers may be specified to limit processing to a range of frames

--- overlay/composite-frame.py
+++ overlay/composite-frame.py
@@ -4,6 +4,8 @@ import os
 import time
 import io
 import re
+import argparse
+import sys
 
 import pystache
 from PIL import Image
@@ -21,6 +23,39 @@ class Data:
         return time.strftime("%I:%M:%S %p", self.time)
 
 
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="The directory that contains the input frames")
+    parser.add_argument("-o", "--output", help="The directory to write the composited frames")
+    parser.add_argument("-s", "--start", type=int, help="The frame number to start processing")
+    parser.add_argument("-e", "--end", type=int, help="The frame number to end processing")
+    args = parser.parse_args()
+
+    if args.input is None:
+        raise ValueError('input directory must be defined')
+    if args.output is None:
+        raise ValueError('output directory must be defined')
+
+    if args.start is None:
+        start = 0
+    else:
+        start = args.start
+
+    if args.end is None:
+        end = sys.maxsize
+    else:
+        end = args.end
+
+    return {
+        "input": args.input,
+        "output": args.output,
+        "start": start,
+        "end": end
+    }
+
+
+options = process_args()
+
 # create renderer
 renderer = pystache.Renderer()
 
@@ -30,8 +65,10 @@ template = template_file.read()
 template_file.close()
 
 # process all files
-frame_dir = '/Users/kevin/Desktop/Second Dive/g2x-1479068752/'
-composite_dir = '/Users/kevin/Desktop/Second Dive/g2x-1479068752-composites/'
+frame_dir = options["input"]
+composite_dir = options["output"]
+start_frame = options["start"]
+end_frame = options["end"]
 
 # create regex for extracting time data from file names
 time_and_frame_pattern = re.compile(r"(\d+)-(\d+)$")
@@ -48,10 +85,13 @@ for frame_file in os.listdir(frame_dir):
     frame_number = float(file_match.group(2))
     frame_time = base_time + frame_number / 24
 
+    if frame_number < start_frame or end_frame < frame_number:
+        continue
+
     print("processing frame {0}...".format(str(int(frame_number))))
 
     # load frame
-    frame_full_path = frame_dir + frame_file
+    frame_full_path = frame_dir + "/" + frame_file
     frame = Image.open(frame_full_path, 'r')
 
     # load data
@@ -72,5 +112,5 @@ for frame_file in os.listdir(frame_dir):
     composite.paste(overlay, (0, 0), overlay)
 
     # output result
-    composite_full_path = composite_dir + frame_no_ext + ".png"
+    composite_full_path = composite_dir + "/" + frame_no_ext + ".png"
     composite.save(composite_full_path, optimize=False)
