0f9168caa5085cb225ea04b725a2379eef8c3b8d
Kevin Lindsey <kevin@kevlindev.com>
2016-11-23 18:54:42 -0800

    Fix some minor formatting issues

--- app/Voltage/voltage.py
+++ app/Voltage/voltage.py
@@ -6,18 +6,20 @@ CMD_READ_ANALOG = 1
 VOLT12 = 650
 VOLT18 = 978
 
+
 def map_range(x, in_min, in_max, out_min, out_max):
     out_delta = out_max - out_min
     in_delta = in_max - in_min
 
     return (x - in_min) * out_delta / in_delta + out_min
 
+
 class Voltage:
     def __init__(self):
         self.bus = smbus.SMBus(1)
 
     def get_data(self):
-        voltage_time = time.time();
+        voltage_time = time.time()
         voltage_raw = self.bus.read_word_data(ADDRESS, CMD_READ_ANALOG)
         voltage = map_range(voltage_raw, VOLT12, VOLT18, 12, 18)
 
