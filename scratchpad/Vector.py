9782b78e4512ee407d60e08eeccca319df4b6907
Kevin Lindsey <kevin@kevlindev.com>
2017-01-18 17:00:22 -0800

    Beginnings of a simple Vector class
    Mainly we need this to keep track of the current joystick positions and we use this to determine the angle of the joystick

---
+++ scratchpad/Vector.py
@@ -0,0 +1,31 @@
+import math
+
+class Vector:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    @property
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+
+    @property
+    def unit(self):
+        length = self.length
+        if length == 0:
+            return Vector()
+        else:
+            return Vector(self.x / length, self.y / length)
+
+    @property
+    def angle(self):
+        unit = self.unit
+        angle = math.atan2(-unit.y, unit.x) * 180.0 / math.pi
+
+        if angle < 0.0:
+            return angle + 360.0
+        else:
+            return angle
+
+    def __str__(self):
+        return "Vector({},{}), angle={}".format(self.x, self.y, self.angle)
