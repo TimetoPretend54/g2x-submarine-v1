87766416ec1bda523a063dba1bbc064355dd711f
Kevin Lindsey <kevin@kevlindev.com>
2017-01-18 17:45:30 -0800

    Control motors via interpolation table
    It works!

--- scratchpad/motors.py
+++ scratchpad/motors.py
@@ -5,11 +5,28 @@ import atexit
 import pygame
 
 from Vector import Vector
+from Interpolator import Interpolator
+
+PRECISION = 3
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT(addr=0x60)
-j1 = Vector()
 j2 = Vector()
+
+# setup left joystick
+j1 = Vector()
+left_thruster = Interpolator()
+left_thruster.addIndexValue(0.0, -1.0)
+left_thruster.addIndexValue(90.0, 1.0)
+left_thruster.addIndexValue(180.0, 1.0)
+left_thruster.addIndexValue(270.0, -1.0)
+left_thruster.addIndexValue(360.0, -1.0)
+right_thruster = Interpolator()
+right_thruster.addIndexValue(0.0, 1.0)
+right_thruster.addIndexValue(90.0, 1.0)
+right_thruster.addIndexValue(180.0, -1.0)
+right_thruster.addIndexValue(270.0, -1.0)
+right_thruster.addIndexValue(360.0, 1.0)
 
 
 # recommended for auto-disabling motors on shutdown!
@@ -18,6 +35,17 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+
+def setMotor(motor_number, value):
+    motor = mh.getMotor(motor_number)
+
+    if value < 0:
+        motor.run(Adafruit_MotorHAT.BACKWARD)
+        motor.setSpeed(-int(value))
+    else:
+        motor.run(Adafruit_MotorHAT.FORWARD)
+        motor.setSpeed(int(value))
 
 
 atexit.register(turnOffMotors)
@@ -37,29 +65,31 @@ done = False
 
 while done is False:
     for event in pygame.event.get():
+        show_thrusters = False
+
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.JOYAXISMOTION:
             if event.axis == 0:
-                value = round(event.value, 2)
+                value = round(event.value, PRECISION)
                 if j1.x != value:
                     j1.x = value
-                    print("j1 = {}".format(j1))
+                    show_thrusters = True
             elif event.axis == 1:
-                value = round(event.value, 2)
+                value = round(event.value, PRECISION)
                 if j1.y != value:
                     j1.y = value
-                    print("j1 = {}".format(j1))
+                    show_thrusters = True
             elif event.axis == 2:
-                value = round(event.value, 2)
+                value = round(event.value, PRECISION)
                 if j2.x != value:
                     j2.x = value
-                    print("j2 = {}".format(j2))
+                    show_thrusters = True
             elif event.axis == 5:
-                value = round(event.value, 2)
+                value = round(event.value, PRECISION)
                 if j2.y != value:
                     j2.y = value
-                    print("j2 = {}".format(j2))
+                    show_thrusters = True
             else:
                 # pass
                 print("unknown axis ", event.axis)
@@ -71,3 +101,11 @@ while done is False:
             print("unhandled hat event")
         elif event.type == pygame.JOYBALLMOTION:
             print("unhandled ball motion event")
+
+        if show_thrusters:
+            left_value = left_thruster.valueAtIndex(j1.angle)
+            right_value = right_thruster.valueAtIndex(j1.angle)
+            power = min(1.0, j1.length)
+            print("left_thruster = {}, right_thruster = {}, power = {}".format(left_value, right_value, power))
+            setMotor(1, left_value * power * 255.0)
+            setMotor(3, right_value * power * 255.0)
